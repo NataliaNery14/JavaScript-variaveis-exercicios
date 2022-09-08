affa71148db5b4b40005afbee42e4bb08a3ce2f4
NataliaNery14 <natalianery@live.com>
2022-09-08 17:23:35 -0300

    variaveis java-script

---
+++ java script.js 4.js
@@ -0,0 +1,8 @@
+// prompt - sync
+// Node Packager manager - npm install prompt - sync 
+const prompt = require('prompt-sync')();
+
+const userName = prompt('Type your name: ');
+
+console.log(`the typed name was: ${userName} `);
+
---
+++ script.js
@@ -0,0 +1,13 @@
+const userName = prompt ('Type your name');
+const lastName = prompt ('Type your Last Name');
+const Weight = prompt ('Type your Weight');
+const height = prompt ('Type your height');
+const admissionDate = prompt ('Type your admission date');
+const diabetic = prompt ('You are diabetic');
+
+console.log(userName);
+console.log(lastName);
+console.log(Weight);
+console.log(height);
+console.log(admissionDate);
+console.log(diabetic);
